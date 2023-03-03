Leo3418/leo3418-ebuild-repo

commit 0fc7f508aeabaf9e6452191ca6ce11407af86917

dev-embedded/fxsdk: Add new ncurses dependency to live ebuild
Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

dev-embedded/fxsdk/fxsdk-9999.ebuild
@@ -32,6 +32,7 @@ BDEPEND="
 
 DEPEND="
 	media-libs/libpng:0/16
+	sys-libs/ncurses:=
 	virtual/libusb:1
 	sdl? (
 		media-libs/libsdl2

commit 345dc9d917db2a72107605b20f1fd140085adea2

dev-embedded/fxsdk: Update copyright year
Fixes: 0fc7f50 ("dev-embedded/fxsdk: Add new ncurses dependency to ...")
Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

dev-embedded/fxsdk/fxsdk-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2022 Gentoo Authors
+# Copyright 2022-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8