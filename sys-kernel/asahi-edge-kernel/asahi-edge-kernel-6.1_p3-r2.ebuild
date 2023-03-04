Leo3418/leo3418-ebuild-repo

commit 579815e6e9f9a008c847991678c5a3633f09e08d

sys-kernel/asahi-edge-kernel: Move Rust toolchain check to pkg_setup
The Rust toolchain required to build the kernel's Rust components may be
installed after this kernel package's pkg_pretend, so checking them in
pkg_pretend is too early.  In a normal package manager execution, the
earliest time when the required Rust toolchain components (as declared
in BDEPEND) should be available is this package's pkg_setup phase.

Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

sys-kernel/asahi-edge-kernel/asahi-edge-kernel-6.1_p3-r2.ebuild
@@ -71,7 +71,7 @@ asahi-kernel_need_rust() {
 	in_iuse gpu-driver && use gpu-driver
 }
 
-pkg_pretend() {
+pkg_setup() {
 	[[ ${MERGE_TYPE} != binary ]] && asahi-kernel_need_rust &&
 		asahi-kernel_check_rust
 }

sys-kernel/asahi-edge-kernel/asahi-edge-kernel-6.2_rc3_p6-r1.ebuild
@@ -71,7 +71,7 @@ asahi-kernel_need_rust() {
 	in_iuse gpu-driver && use gpu-driver
 }
 
-pkg_pretend() {
+pkg_setup() {
 	[[ ${MERGE_TYPE} != binary ]] && asahi-kernel_need_rust &&
 		asahi-kernel_check_rust
 }

commit 557dbc281c4fa79480ea2bb6bb4f2b0f7fe65034

sys-kernel/asahi-edge-kernel: Restore python-any-r1_pkg_setup invocation
The default pkg_setup of an ebuild that inherits only
kernel-build.eclass should be python-any-r1_pkg_setup.

Fixes: 579815e (sys-kernel/asahi-edge-kernel: Move Rust toolchain check to pkg_setup, 2023-03-04)
Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

sys-kernel/asahi-edge-kernel/asahi-edge-kernel-6.1_p3-r2.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit kernel-build savedconfig toolchain-funcs
+inherit kernel-build python-any-r1 savedconfig toolchain-funcs
 
 # https://github.com/AsahiLinux/PKGBUILDs/blob/main/linux-asahi/config
 PKGBUILD_CONFIG_COMMIT="da67cf02622899775c233f52e441fc8127f51d99"
@@ -72,6 +72,7 @@ asahi-kernel_need_rust() {
 }
 
 pkg_setup() {
+	python-any-r1_pkg_setup
 	[[ ${MERGE_TYPE} != binary ]] && asahi-kernel_need_rust &&
 		asahi-kernel_check_rust
 }

sys-kernel/asahi-edge-kernel/asahi-edge-kernel-6.2_rc3_p6-r1.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit kernel-build savedconfig toolchain-funcs
+inherit kernel-build python-any-r1 savedconfig toolchain-funcs
 
 # https://github.com/AsahiLinux/PKGBUILDs/blob/main/linux-asahi/config
 PKGBUILD_CONFIG_COMMIT="fa0fbb251bbe98a2d7977b2854b07685b0acd18c"
@@ -72,6 +72,7 @@ asahi-kernel_need_rust() {
 }
 
 pkg_setup() {
+	python-any-r1_pkg_setup
 	[[ ${MERGE_TYPE} != binary ]] && asahi-kernel_need_rust &&
 		asahi-kernel_check_rust
 }